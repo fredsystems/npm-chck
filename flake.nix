dbeee8fa40c303d3f7aa826d6e30a6353763ac81
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2026-02-18 11:55:53 -0700

    flake: add packages output for installation via flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,6 @@
 {
-  # This example flake.nix is pretty generic and the same for all
-  # examples, except when they define devShells or extra packages.
-  description = "Dev Flake";
+  description = "npm-chck - Check for outdated, incorrect, and unused dependencies.";
 
-  # We import the latest commit of dream2nix main branch and instruct nix to
-  # reuse the nixpkgs revision referenced by dream2nix.
-  # This is what we test in CI with, but you can generally refer to any
-  # recent nixpkgs commit here.
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -23,8 +17,6 @@
       nixpkgs,
     }:
     let
-      # A helper that helps us define the attributes below for
-      # all systems we care about.
       eachSystem = nixpkgs.lib.genAttrs [
         "aarch64-darwin"
         "aarch64-linux"
@@ -33,6 +25,36 @@
       ];
     in
     {
+      packages = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          npm-chck = pkgs.buildNpmPackage {
+            pname = "npm-chck";
+            version = "7.0.0";
+
+            src = ./.;
+
+            npmDepsHash = "sha256-H2ArXqjZvjJsrICxd9zwC4l9IBKvq94VArkFk84RKyU=";
+
+            # No build step needed for this package
+            npmBuildScript = "prepare";
+
+            meta = with pkgs.lib; {
+              description = "Check for outdated, incorrect, and unused dependencies.";
+              homepage = "https://github.com/FredSystems/npm-chck";
+              license = licenses.mit;
+              maintainers = [ ];
+              mainProgram = "npm-chck";
+            };
+          };
+
+          default = self.packages.${system}.npm-chck;
+        }
+      );
+
       checks = eachSystem (system: {
         pre-commit-check = precommit-base.lib.mkCheck {
           inherit system;
@@ -42,12 +64,6 @@
           check_javascript = true;
           check_python = false;
 
-          # javascript = {
-          #   enableBiome = true;
-          #   enableTsc = true;
-          #   tsConfig = "./tsconfig.json";
-          # };
-
           extraExcludes = [
             "secrets.yaml"
             "Logo-Sources"
@@ -56,26 +72,6 @@
         };
       });
 
-      # packages = eachSystem (system: {
-      #   # For each system, we define our default package
-      #   # by passing in our desired nixpkgs revision plus
-      #   # any dream2nix modules needed by it.
-      #   dream2nix.lib.evalModules {
-      #     packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-      #     modules = [
-      #       # Import our actual package definition as a dream2nix module from ./default.nix
-      #       ./default.nix
-      #       {
-      #         # Aid dream2nix to find the project root. This setup should also works for mono
-      #         # repos. If you only have a single project, the defaults should be good enough.
-      #         paths.projectRoot = ./.;
-      #         # can be changed to ".git" or "flake.nix" to get rid of .project-root
-      #         paths.projectRootFile = "flake.nix";
-      #         paths.package = ./.;
-      #       }
-      #     ];
-      #   };
-      # );
       devShells = eachSystem (
         system:
         let
